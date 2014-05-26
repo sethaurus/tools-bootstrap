552a3b0d765a77440f2d7211dce9d1a73a798faf
Henry Greville <20176974@ecm-csse2124-x.uniwa.uwa.edu.au>
2014-05-26 08:51:15 +0800

    For real this time

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 fn=/tmp/$RANDOM
 touch $fn
 chmod +x $fn
-curl https://raw.githubusercontent.com/sethaurus/tools-bootstrap/master/interactive.sh > $fn
+curl -L https://raw.githubusercontent.com/sethaurus/tools-bootstrap/master/interactive.sh > $fn
 bash $fn
