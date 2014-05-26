c99ee9383e524a88a2506ae667cb23b3a75a41de
Henry Greville <20176974@ecm-csse2124-x.uniwa.uwa.edu.au>
2014-05-26 08:48:51 +0800

    Added non-interactive bootstrap file, hopefully to allow piping

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,5 @@
-clear
-key_location="$HOME/.ssh/id_rsa"
-if ! [[ -e $key_location.pub ]]; then
-	ssh-keygen -q -t rsa -f $key_location -N ""
-fi
-
-key_data=$(cat $key_location.pub)
-key_name=$HOSTNAME:$RANDOM
-json_payload="{\"title\": \"$key_name\", \"key\": \"$key_data\"}"
-read -s -p "Github password? " github_password
-github_username=sethaurus
-github_url="https://$github_username:$github_password@api.github.com/user/keys"
-
-curl --data "$json_payload" $github_url
-cd ~
-yes | git clone ssh://git@github.com/$github_username/tools
+fn=/tmp/$RANDOM
+touch $fn
+chmod +x $fn
+curl https://raw.githubusercontent.com/sethaurus/tools-bootstrap/master/interactive.sh > $fn
+bash $fn
